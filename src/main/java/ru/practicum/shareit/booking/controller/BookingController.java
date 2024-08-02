859ec106cbe483498c538c856ab2fd821f99e4e4
Georgii <moroz.georgijs@yandex.ru>
2024-08-02 11:27:44 +0300

    7

--- src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.dto.BookingDto;
-import ru.practicum.shareit.booking.service.BookingService;
+import ru.practicum.shareit.booking.service.BookingServiceImpl;
 
 import java.util.List;
 
@@ -16,7 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 public class BookingController {
-    private final BookingService service;
+    private final BookingServiceImpl service;
 
     @PostMapping
     public ResponseEntity<BookingDto> requestBooking(@RequestBody @Valid BookingDto bookingDto,
